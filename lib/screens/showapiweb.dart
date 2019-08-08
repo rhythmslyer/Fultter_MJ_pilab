9cf78773028dd73d86b412ff0b828ade15e7af06
unknown <tanawats@gcnetbkk.com>
2019-08-08 16:02:44 +0700

    GET API AND SHOP DATA BY MODEL

--- lib/screens/showapiweb.dart
+++ lib/screens/showapiweb.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
+import 'package:mj_pilab/model/profile.dart';
 
 class ShowApiWeb extends StatefulWidget {
   @override
@@ -13,19 +13,21 @@ class ShowApiWeb extends StatefulWidget {
 class _ShowApiWebState extends State<ShowApiWeb> {
   // Expilicit
   String urlAPI = 'https://jsonplaceholder.typicode.com/photos';
-  
+  List<ProfileModel> profiles = new List<ProfileModel>();
 
   // Method
-  Future<void> readApi() async{
-    print('Start Get API');
-     var respones = await http.get(urlAPI);
-     if (respones.statusCode == 200) {
-       var result = json.decode(respones.body);
-       print('result : $result');
-
-       
-     }
-
+  Future<void> readApi() async {
+    var respones = await http.get(urlAPI);
+    if (respones.statusCode == 200) {
+      var result = json.decode(respones.body);
+      // print('result : $result');
+      for (var profile in result) {
+        ProfileModel profileModel = ProfileModel.formAPI(profile);
+        setState(() {
+          profiles.add(profileModel);
+        });
+      }
+    }
   }
 
   @override
@@ -34,8 +36,51 @@ class _ShowApiWebState extends State<ShowApiWeb> {
     readApi();
   }
 
+  Widget myListview(int index) {
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width * 0.4,
+            child: Image.network(profiles[index].thumbnailUrl),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.4,
+            child: Text(profiles[index].title),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('This is aBook');
+    return ListView.builder(
+      itemCount: profiles.length,
+      itemBuilder: (BuildContext context, int index) {
+        return GestureDetector(
+          child: myListview(index),
+          onTap: () {
+            // print('your click : ${profiles[index].title}');
+            printModel(profiles[index]);
+          },
+        );
+      },
+    );
   }
+}
+
+void printModel(ProfileModel model){
+  int albumId = model.albumId;
+  int id = model.id;
+  String title = model.title;
+  String url = model.url;
+  String thumbnailUrl = model.thumbnailUrl;
+  print('albumId : $albumId');
+  print('id : $id');
+  print('title : $title');
+  print('url : $url');
+  print('thumbnailUrl : $thumbnailUrl');
+
+
 }
